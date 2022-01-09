cb591570cd39d9041f4e51f7233fda247ff18987
bjval <bjval@DESKTOP-T8601U9>
2022-01-09 13:31:05 -0800

    Add project files.

---
+++ ASPNETMVCRouting/App_Start/RouteConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ASPNETMVCRouting
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
+
+            
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            
+
+            routes.MapRoute("Blog", "blog/{year}/{month}/{day}",
+                            new { Controller = "Blog", action = "Index" , day = UrlParameter.Optional},
+                            new { year = @"\d{4}", month = @"\d{2}"}
+            );
+
+        }
+    }
+}
---
+++ ASPNETMVCRouting/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNETMVCRouting.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index(int year, int month, int day)
+        {
+            ViewBag.Year = year.ToString();
+            ViewBag.Month = month.ToString();
+            ViewBag.Day = day;
+            return View();
+        }
+    }
+}
